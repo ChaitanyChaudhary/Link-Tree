3d96e7079831a2b6d9b045bb3a50663e90263f5f
Chaitany Chaudhary <85825826+ChaitanyChaudhary@users.noreply.github.com>
2024-12-30 18:03:51 +0530

    Initial commit

---
+++ js/links.js
@@ -0,0 +1,22 @@
+export const links = [
+    {
+        name: "Linkedin",
+        link: "https://www.linkedin.com/in/chaitanychaudhary/",
+        image: "/assets/linkedin.png"
+    },
+    {
+        name: "X",
+        link: "https://x.com/Chaitany025",
+        image: "/assets/x.png"
+    },
+    {
+        name: "GitHub",
+        link: "https://github.com/ChaitanyChaudhary/",
+        image: "/assets/github.png"
+    },
+    {
+        name: "Other",
+        link: "https://www.other.com/chaitanychaudhary/",
+        image: "/assets/linkedin.png"
+    },
+];
